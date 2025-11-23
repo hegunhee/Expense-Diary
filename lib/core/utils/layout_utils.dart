cb3d01846acdd3b8389693b9dae0460e020ed507
이건희 <leech9876@naver.com>
2025-11-23 21:55:51 +0900

    [#13] feat : 시스템 네비게이션 바의 크기를 측정해주는 함수 작성
    안드로이드에서 사용하는 systemBarsPadding과 이름을 동일하게 만들었습니다

---
+++ lib/core/utils/layout_utils.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+/// 시스템 하단 네비게이션바의 패딩을 측정해주는 함수 (추가적으로 하단에 한해서 extra 값을 추가 가능)
+EdgeInsets systemBarsPadding(BuildContext context, {double extra = 0}) {
+  final padding = MediaQuery.of(context).viewPadding;
+  return EdgeInsets.only(bottom: padding.bottom + extra);
+}
