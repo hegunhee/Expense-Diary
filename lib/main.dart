462ef4d90837e17793387a4b3c5e6ebe26befc77
이건희 <leech9876@naver.com>
2025-12-04 00:07:01 +0900

    [#17] feat : firebase 설정, crashlytics도 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'dart:async';
+import 'dart:developer';
+import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:expense_tracker/core/controllers/theme_controller.dart';
 import 'package:expense_tracker/core/themes/app_theme.dart';
-import 'package:expense_tracker/features/expense/models/expense.dart';
 import 'package:expense_tracker/features/expense/screens/expense_list_screen.dart';
+import 'package:expense_tracker/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:expense_tracker/firebase_options.dart';
 
 /// 앱 시작점
 void main() {
@@ -17,36 +19,26 @@ void main() {
     () async {
       WidgetsFlutterBinding.ensureInitialized();
 
-      // Hive 초기화
-      await Hive.initFlutter();
-
-      // Hive 어댑터 등록
-      Hive.registerAdapter(ExpenseCategoryAdapter());
-      Hive.registerAdapter(ExpenseEmotionsAdapter());
-      Hive.registerAdapter(ExpenseAdapter());
-
-      await Hive.openBox<Expense>('expenses');
-
       // Firebase 초기화 (iOS용 GoogleService-Info.plist 필요)
-      // try {
-      //   await Firebase.initializeApp(
-      //     options: DefaultFirebaseOptions.currentPlatform,
-      //   );
+      try {
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
 
-      //   // Crashlytics 설정
-      //   FlutterError.onError = (errorDetails) {
-      //     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-      //   };
+        // Crashlytics 설정
+        FlutterError.onError = (errorDetails) {
+          FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+        };
 
-      //   // 비동기 에러 처리
-      //   PlatformDispatcher.instance.onError = (error, stack) {
-      //     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      //     return true;
-      //   };
-      // } catch (e) {
-      //   // Firebase 설정 실패 시 무시 (iOS 시뮬레이터 등)
-      //   print('Firebase initialization skipped: $e');
-      // }
+        // 비동기 에러 처리
+        PlatformDispatcher.instance.onError = (error, stack) {
+          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+          return true;
+        };
+      } on FirebaseException catch (e) {
+        // Firebase 설정 실패 시 무시 (iOS 시뮬레이터 등)
+        log('Firebase initialization skipped: $e');
+      }
 
       await EasyLocalization.ensureInitialized();
       await GoogleFonts.pendingFonts();
