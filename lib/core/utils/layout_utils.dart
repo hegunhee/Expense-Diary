003982c91ef73856c03e7f2fb5d2e09b4c51231b
이건희 <leech9876@naver.com>
2026-02-14 16:11:47 +0900

    #26 - feat : 키보드가 들어올려졌을때는 하단 패딩이 적용되지않도록 설정

--- lib/core/utils/layout_utils.dart
+++ lib/core/utils/layout_utils.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 /// 시스템 하단 네비게이션바의 패딩을 측정해주는 함수 (추가적으로 하단에 한해서 extra 값을 추가 가능)
+/// 키보드가 올라왔을 때는 하단 패딩을 적용하지 않음
 EdgeInsets systemBarsPadding(BuildContext context, {double extra = 0}) {
   final padding = MediaQuery.of(context).viewPadding;
-  return EdgeInsets.only(bottom: padding.bottom + extra);
+  final viewInsets = MediaQuery.of(context).viewInsets;
+
+  // 키보드가 올라왔을 때는 하단 패딩 제거
+  final bottomPadding = viewInsets.bottom > 0 ? 0.0 : padding.bottom + extra;
+
+  return EdgeInsets.only(bottom: bottomPadding);
 }
