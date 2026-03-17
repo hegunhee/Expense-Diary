274e134250128ecc456ccff9027e81ac7c4b89e7
이건희 <leech9876@naver.com>
2026-03-17 21:28:28 +0900

    #35 - feat : DatePicker 색상 지정을 위해 app_theme에 DatePickerThemeData 지정

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -3,33 +3,36 @@ import 'package:flutter/material.dart';
 
 /// 앱 전체에서 사용되는 테마 설정을 정의하는 클래스
 class AppTheme {
+
   /// 라이트 모드 테마 설정을 반환합니다.
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
       brightness: Brightness.light,
-      colorScheme: const ColorScheme.light(
-        primary: Color(0xFF2196F3),
-        surface: Color(0xFFF5F5F5),
+      /// datePicker는 고유 Theme 데이터의 색상을 가지므로 설정
+      datePickerTheme: DatePickerThemeData(
+        todayForegroundColor: WidgetStateProperty.resolveWith((states) {
+          if(states.contains(WidgetState.selected)) {
+            return Colors.white;
+          }
+          return Colors.red;
+        }),
+        todayBackgroundColor: WidgetStateProperty.resolveWith((states) {
+          if(states.contains(WidgetState.selected)) {
+            return const Color(0xFF4CAF50);
+          }
+          return Colors.white;
+        }),
+        todayBorder: BorderSide.none,
+        dayBackgroundColor: WidgetStateProperty.resolveWith((states) {
+          if(states.contains(WidgetState.selected)) {
+            return const Color(0xFF4CAF50);
+          }
+          return null;
+        })
       ),
       extensions: const <ThemeExtension<dynamic>>[
         AppColors.light,
-      ],
-    );
-  }
-
-  /// 다크 모드 테마 설정을 반환합니다.
-  static ThemeData get darkTheme {
-    return ThemeData(
-      useMaterial3: true,
-      brightness: Brightness.dark,
-      colorScheme: const ColorScheme.dark(
-        primary: Color(0xFF64B5F6),
-        secondary: Color(0xFF26C6DA),
-        surface: Color(0xFF1E1E1E),
-      ),
-      extensions: const <ThemeExtension<dynamic>>[
-        AppColors.dark,
       ],
     );
   }
