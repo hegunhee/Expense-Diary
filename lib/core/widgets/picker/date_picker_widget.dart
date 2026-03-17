c2b36834cd7f1a714df160bf99cb3d99e3a8497c
이건희 <leech9876@naver.com>
2026-03-17 21:41:31 +0900

    #35 - feat : 날짜 피커 작업
    높이는 적당히 250으로 설정

---
+++ lib/core/widgets/picker/date_picker_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+///  날짜 피커 위젯
+class DatePickerWidget extends StatelessWidget {
+  /// 날짜 피커 위젯 생성자
+  const DatePickerWidget({
+    required this.selectedDate,
+    required this.onDateChanged,
+    super.key,
+  });
+
+  /// 선택된 날짜
+  final DateTime selectedDate;
+
+  /// 날짜가 변경될 때 콜백
+  final ValueChanged<DateTime> onDateChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+        child: CalendarDatePicker(
+          initialDate: selectedDate,
+          /// 임의로 2020년부터 2100년 사이의 값을 가지도록 설정
+          firstDate: DateTime(2020),
+          lastDate: DateTime(2100),
+          onDateChanged: onDateChanged,
+        ),
+    );
+  }
+}
